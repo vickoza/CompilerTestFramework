1c8b7063f30c6464ed083c23c9071d7524d6c161
Vishal Oza <Vishal.Oza@amsusa1.onmicrosoft.com>
2020-10-21 10:44:20 -0500

    updates for version 5 preview

--- WindowsCompilerTestFramework/WindowsCompilerTestFramework.cpp
+++ WindowsCompilerTestFramework/WindowsCompilerTestFramework.cpp
@@ -6,13 +6,13 @@
 
 int main()
 {
-	std::vector<std::string> setup = { "set __VSCMD_PREINIT_INCLUDE=\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29304/include\";\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/SDK/ScopeCppSDK/vc15/SDK/include/ucrt\"" };
+	std::vector<std::string> setup = { "set __VSCMD_PREINIT_INCLUDE=\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29333/include\";\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/SDK/ScopeCppSDK/vc15/SDK/include/ucrt\";\"C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/ucrt/stddef.h\"" };
 	std::vector<std::string> setup2 = {  };
 	std::vector<std::string> flags = { "std:c++latest", "W4", "nologo", "c", "EHsc" };
 	std::vector<std::string> flags2 = { "std=c++20", "Wextra", "S" };
-	std::vector<std::string> includes = { "\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29304/include\"", "\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/SDK/ScopeCppSDK/vc15/VC/include\"", "\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/SDK/ScopeCppSDK/vc15/SDK/include/ucrt\"" };
+	std::vector<std::string> includes = { "\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29333/include\"","\"C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/ucrt\"", "\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/SDK/ScopeCppSDK/vc15/VC/include\"","\"C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/ucrt\"", "\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/SDK/ScopeCppSDK/vc15/SDK/include/ucrt\"" };
 	std::vector<std::string> includes2 = {  };
-	std::vector<std::string> includes3 = { "\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29304/include\"", "\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/SDK/ScopeCppSDK/vc15/VC/include\"", "\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/SDK/ScopeCppSDK/vc15/SDK/include/ucrt\"", "C:/Users/Vishal/source/repos/timeContainer/timeContainer" };
+	std::vector<std::string> includes3 = { "\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29333/include\"","\"C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/ucrt\"", "\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/SDK/ScopeCppSDK/vc15/VC/include\"", "\"C:/Program Fils (x86)/Microsoft Visual Studio/2019/Preview/SDK/ScopeCppSDK/vc15/SDK/include/ucrt\"", "C:/Users/Vishal/source/repos/timeContainer/timeContainer" };
 	std::vector<std::string> includes4 = { "C:/Users/Vishal/source/repos/timeContainer/timeContainer" };
 
 	auto code =
@@ -135,48 +135,48 @@ int main()
 }
 )";
 
-	std::cout << "Code 1\n";
-	auto [a, b] = compilerCode(compilerSystem::Visual_C, code, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29304/bin/Hostx86/x86/cl", setup, flags, includes);
+	/*std::cout << "Code 1\n";
+	auto [a, b] = compilerCode(compilerSystem::Visual_C, code, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29333/bin/Hostx86/x86/cl", setup, flags, includes);
 	std::cout << "VC++ " << std::boolalpha << a << " " << b << " warning\n";
 	auto [c, d] = compilerCode(compilerSystem::Clang_Win, code, (std::string)"C:/Program Files/LLVM/bin/clang++", setup2, flags2, includes2);
 	std::cout << "clang++ " << std::boolalpha << c << " " << d << " warning\n";
-	std::cout << "\nCode 2\n";
-	auto [e, f] = compilerCode(compilerSystem::Visual_C, code2, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29304/bin/Hostx64/x64/cl", setup, flags, includes);
+	*/std::cout << "\nCode 2\n";
+	auto [e, f] = compilerCode(compilerSystem::Visual_C, code2, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29333/bin/Hostx86/x86/cl", setup, flags, includes);
 	std::cout << "VC++ " << std::boolalpha << e << " " << f << " warning\n";
 	auto [g, h] = compilerCode(compilerSystem::Clang_Win, code2, (std::string)"C:/Program Files/LLVM/bin/clang++", setup2, flags2, includes2);
 	std::cout << "clang++ " << std::boolalpha << g << " " << h << " warning\n";
 	std::cout << "\nCode 3\n";
-	auto [i, j] = compilerCode(compilerSystem::Visual_C, code3, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29304/bin/Hostx64/x64/cl", setup, flags, includes);
+	auto [i, j] = compilerCode(compilerSystem::Visual_C, code3, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29333/bin/Hostx64/x64/cl", setup, flags, includes);
 	std::cout << "VC++ " << std::boolalpha << i << " " << j << " warning\n";
 	auto [k, l] = compilerCode(compilerSystem::Clang_Win, code3, (std::string)"C:/Program Files/LLVM/bin/clang++", setup2, flags2, includes2);
 	std::cout << "clang++ " << std::boolalpha << k << " " << l << " warning\n";
 	std::cout << "\nCode 4\n";
-	auto [m, n] = compilerCode(compilerSystem::Visual_C, code4, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29304/bin/Hostx64/x64/cl", setup, flags, includes3);
+	auto [m, n] = compilerCode(compilerSystem::Visual_C, code4, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29333/bin/Hostx64/x64/cl", setup, flags, includes3);
 	std::cout << "VC++ " << std::boolalpha << m << " " << n << " warning\n";
 	auto [o, p] = compilerCode(compilerSystem::Clang_Win, code4, (std::string)"C:/Program Files/LLVM/bin/clang++", setup2, flags2, includes4);
 	std::cout << "clang++ " << std::boolalpha << o << " " << p << " warning\n";
 	std::cout << "\nCode 5\n";
-	auto [q, r] = compilerCode(compilerSystem::Visual_C, code5, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29304/bin/Hostx64/x64/cl", setup, flags, includes3);
+	auto [q, r] = compilerCode(compilerSystem::Visual_C, code5, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29333/bin/Hostx64/x64/cl", setup, flags, includes3);
 	std::cout << "VC++ " << std::boolalpha << q << " " << r << " warning\n";
 	auto [s, t] = compilerCode(compilerSystem::Clang_Win, code5, (std::string)"C:/Program Files/LLVM/bin/clang++", setup2, flags2, includes4);
 	std::cout << "clang++ " << std::boolalpha << s << " " << t << " warning\n";
 	std::cout << "\nCode 6\n";
-	auto [u, v] = compilerCode(compilerSystem::Visual_C, code6, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29304/bin/Hostx64/x64/cl", setup, flags, includes3);
+	auto [u, v] = compilerCode(compilerSystem::Visual_C, code6, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29333/bin/Hostx64/x64/cl", setup, flags, includes3);
 	std::cout << "VC++ " << std::boolalpha << u << " " << v << " warning\n";
 	auto [w, x] = compilerCode(compilerSystem::Clang_Win, code6, (std::string)"C:/Program Files/LLVM/bin/clang++", setup2, flags2, includes4);
 	std::cout << "clang++ " << std::boolalpha << w << " " << x << " warning\n";
 	std::cout << "\nCode 7\n";
-	auto [y, z] = compilerCode(compilerSystem::Visual_C, code7, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29304/bin/Hostx64/x64/cl", setup, flags, includes3);
+	auto [y, z] = compilerCode(compilerSystem::Visual_C, code7, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29333/bin/Hostx64/x64/cl", setup, flags, includes3);
 	std::cout << "VC++ " << std::boolalpha << y << " " << z << " warning\n";
 	auto [a1, b1] = compilerCode(compilerSystem::Clang_Win, code7, (std::string)"C:/Program Files/LLVM/bin/clang++", setup2, flags2, includes4);
 	std::cout << "clang++ " << std::boolalpha << a1 << " " << b1 << " warning\n";
 	std::cout << "\nCode 8\n";
-	auto [c1, d1] = compilerCode(compilerSystem::Visual_C, code8, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29304/bin/Hostx64/x64/cl", setup, flags, includes3);
+	auto [c1, d1] = compilerCode(compilerSystem::Visual_C, code8, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29333/bin/Hostx64/x64/cl", setup, flags, includes3);
 	std::cout << "VC++ " << std::boolalpha << c1 << " " << d1 << " warning\n";
 	auto [e1, f1] = compilerCode(compilerSystem::Clang_Win, code8, (std::string)"C:/Program Files/LLVM/bin/clang++", setup2, flags2, includes4);
 	std::cout << "clang++ " << std::boolalpha << e1 << " " << f1 << " warning\n";
 	std::cout << "\nCode 9\n";
-	auto [g1, h1] = compilerCode(compilerSystem::Visual_C, code9, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29304/bin/Hostx64/x64/cl", setup, flags, includes3);
+	auto [g1, h1] = compilerCode(compilerSystem::Visual_C, code9, (std::string)"C:/Program Files (x86)/Microsoft Visual Studio/2019/Preview/VC/Tools/MSVC/14.28.29333/bin/Hostx64/x64/cl", setup, flags, includes3);
 	std::cout << "VC++ " << std::boolalpha << g1 << " " << h1 << " warning\n";
 	auto [i1, j1] = compilerCode(compilerSystem::Clang_Win, code9, (std::string)"C:/Program Files/LLVM/bin/clang++", setup2, flags2, includes4);
 	std::cout << "clang++ " << std::boolalpha << i1 << " " << j1 << " warning\n";
